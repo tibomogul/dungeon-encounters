3c4d1ee4b620aa70c9605e830d905150420d7090
Tibo Mogul <tibo.mogul@gmail.com>
2023-01-12 21:51:34 +1000

    prefab

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -3,11 +3,13 @@ use crate::prelude::*;
 mod automata;
 mod drunkard;
 mod empty;
+mod prefab;
 mod rooms;
 
 // use empty::EmptyArchitect;
 use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
+use prefab::apply_prefab;
 use rooms::RoomsArchitect;
 
 trait MapArchitect {
@@ -26,12 +28,13 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
-            0 => Box::new(DrunkardsWalkArchitect {}),
-            1 => Box::new(RoomsArchitect {}),
-            _ => Box::new(CellularAutomataArchitect {}),
+        let mut architect : Box<dyn MapArchitect> = match rng.range(0, 3) {
+        0 => Box::new(DrunkardsWalkArchitect{}),
+        1 => Box::new(RoomsArchitect{}),
+        _ => Box::new(CellularAutomataArchitect{})
         };
         let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
         mb
     }
     fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
